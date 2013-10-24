259d43a97140e6f1af565322ea1da4e1f1fa42e1
Juliana Nazare <jules.nazare@gmail.com>
2013-10-23 23:36:45 -0400

    disabling buttons

--- public/javascripts/render_markdown.js
+++ public/javascripts/render_markdown.js
@@ -1,4 +1,5 @@
 $( document ).ready(function() {
+	$("#edit-button").attr("disabled", "disabled");
 	var editor = ace.edit("editor");
 	editor.setFontSize(Number("10"));
 	var code = editor.getSession().getValue();
--- public/javascripts/toggle.js
+++ public/javascripts/toggle.js
@@ -1,16 +1,38 @@
 $( "#edit-button" ).click(function() {
 	$('#edit_foreditcontent').css('display', 'block');
 	$('#view_foreditcontent').css('display', 'none');
-	$('#asset-mask').css('display', 'none');
+	$('#asset-mask').css('display', 'block');
+	$("#edit-button").attr("disabled", "disabled");
+	$("#view-button").removeAttr("disabled", "disabled");
+	$("#h1-button").removeAttr("disabled", "disabled");
+	$("#h2-button").removeAttr("disabled", "disabled");
+	$("#h3-button").removeAttr("disabled", "disabled");
+	$("#bold-button").removeAttr("disabled", "disabled");
+	$("#italic-button").removeAttr("disabled", "disabled");
+	$("#link-button").removeAttr("disabled", "disabled");
+	$("#bullet-button").removeAttr("disabled", "disabled");
+	$("#code-button").removeAttr("disabled", "disabled");
+	$("#upload-button").removeAttr("disabled", "disabled");
 	});
 $( "#view-button" ).click(function() {
 	var editor = ace.edit("editor");
 	var code = editor.getSession().getValue();
-	  $('#view_foreditcontent').css('display', 'block');
-	  $('#edit_foreditcontent').css('display', 'none');
-	  $('#asset-mask').css('display', 'none');
-	  $('#view_foreditcontent').html(marked(code));
-	  $('#content_to_save').val(code);
+	$('#view_foreditcontent').css('display', 'block');
+	$('#edit_foreditcontent').css('display', 'none');
+	$('#asset-mask').css('display', 'none');
+	$('#view_foreditcontent').html(marked(code));
+	$('#content_to_save').val(code);
+	$("#edit-button").removeAttr("disabled", "disabled");
+	$("#view-button").attr("disabled", "disabled");
+	$("#h1-button").attr("disabled", "disabled");
+	$("#h2-button").attr("disabled", "disabled");
+	$("#h3-button").attr("disabled", "disabled");
+	$("#bold-button").attr("disabled", "disabled");
+	$("#italic-button").attr("disabled", "disabled");
+	$("#link-button").attr("disabled", "disabled");
+	$("#bullet-button").attr("disabled", "disabled");
+	$("#code-button").attr("disabled", "disabled");
+	$("#upload-button").attr("disabled", "disabled");
 });
 $( "#save-button" ).click(function() {
 	var editor = ace.edit("editor");
