ad5c186bc2be213fa435d97486c07c79d23a5588
Juliana Nazare <jules.nazare@gmail.com>
2013-11-05 17:07:16 -0500

    make new lab

--- routes/helpers.js
+++ routes/helpers.js
@@ -3,8 +3,8 @@ var fs = require( 'fs' );
 var async = require( 'async' );
 
 exports.newlab = function(req, callback){
-	this.get_file_structure(req, function(files){
-		this.get_file(req, function(result){ 
+	exports.get_file_structure(req, function(files){
+		exports.get_file(req, function(result){ 
 			callback.render('index', {
 				logged_in: 'true',
 				root: './public', 
